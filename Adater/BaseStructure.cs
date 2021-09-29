7647d91be26056e88440deef12f7be725c253d7a
Кирилл <kattoshkin@gmail.com>
2021-09-30 00:08:45 +0700

    feat: add Adater

---
+++ Adater/BaseStructure.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    class BaseStructure
+    {
+    }
+    /// <summary>
+    /// Использует объекты Target для выполнения своих задач
+    /// </summary>
+    class Client
+    {
+        public void Request(Target target)
+        {
+            target.Request();
+        }
+    }
+    // класс, к которому надо адаптировать другой класс
+    /// <summary>
+    /// Представляет объекты, которые используются клиентом
+    /// </summary>
+    class Target
+    {
+        public virtual void Request()
+        { }
+    }
+
+    // Адаптер
+    /// <summary>
+    /// Собственно адаптер, который позволяет работать с объектами Adaptee как с объектами Target
+    /// </summary>
+    class Adapter : Target
+    {
+        private Adaptee adaptee = new Adaptee();
+
+        public override void Request()
+        {
+            adaptee.SpecificRequest();
+        }
+    }
+
+    // Адаптируемый класс
+    /// <summary>
+    /// Представляет адаптируемый класс, который мы хотели бы использовать у клиента вместо объектов Target
+    /// </summary>
+    class Adaptee
+    {
+        public void SpecificRequest()
+        { }
+    }
+}
