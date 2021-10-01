6ca08dc0520027c0fc56df2df4886a05b03d10d5
Кирилл <kattoshkin@gmail.com>
2021-10-01 23:46:48 +0700

    feat: add Poxy

---
+++ roxy/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Proxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    class Page
+    {
+        public int Id { get; set; }
+        public int Number { get; set; }
+        public string Text { get; set; }
+    }
+}
